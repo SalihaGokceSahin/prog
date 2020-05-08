ee3b5b57e58c5350af313c2dc3054548f7f95119
saliha gokce sahin <gb141216@outlook.com>
2020-05-08 22:15:26 +0300

    ilkversiyon

--- App1/js/main.js
+++ App1/js/main.js
@@ -1,1 +1,36 @@
-﻿// Your code here!
+﻿window.onload = function () {
+    var pos = 0;
+    var posy = 150;
+    var box = document.getElementById("box");
+    var t = setInterval(move, 10);
+    function move() {
+        if (pos >= 150) {
+            posy -= 1;
+            box.style.bottom = posy + "px";
+            if (posy <= 0) {
+                clearInterval(t);
+            }
+
+        }
+        else {
+            pos += 1;
+            box.style.left = pos + "px";
+        }
+        //function move2() {
+        //    if (pos >= 150) {
+        //        pos2=
+        //    }
+        //}
+    }
+    //var pos2 = 0;
+    //var z = setInterval(movee, 10);
+    //    function movee() {
+    //        if (pos >= 100) {
+    //            clearInterval(z);
+    //        }
+    //        else {
+    //            pos2 += 1;
+    //            box.style.bottom= pos2 + "px";
+    //        }
+    //    }
+}
